ea5407f05c830b2e381fbd9f084a05cc7e925c6e
Clay Arizala <clay.arizala@pucese.edu.ec>
2020-06-15 09:17:23 -0500

    Agregar archivos de proyecto.

---
+++ TareadeListas/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TareadeListas.Modelo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombres = new System.Windows.Forms.Label();
+            this.txtApellidos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.AutoSize = true;
+            this.txtNombres.Location = new System.Drawing.Point(35, 22);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(18, 13);
+            this.txtNombres.TabIndex = 0;
+            this.txtNombres.Text = "txt";
+            this.txtNombres.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.AutoSize = true;
+            this.txtApellidos.Location = new System.Drawing.Point(35, 55);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(35, 13);
+            this.txtApellidos.TabIndex = 1;
+            this.txtApellidos.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtNombres;
+        private System.Windows.Forms.Label txtApellidos;
+    }
+}
+
---
+++ TareadeListas/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareadeListas.Modelo
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+{
+    public partial class Form1 : Form
+{
+    private List<Estudiantes> lista = new List<Estudiantes>();
+    private List<Estudiantes> listaGrid = new List<Estudiantes>();
+
+    public Form1()
+    {
+@@ -57,11 + 57,17 @@ private void btnAgregar_Click(object sender, EventArgs e)
+                alumno.nombres = this.txtNombres.Text;
+                alumno.edad = edad;
+                alumno.sexo = this.cmbSexo.Text;
+                listaGrid.Add(alumno);
+
+                lista.Add(alumno);
+                ListViewItem listaView = new ListViewItem(this.txtMatricula.Text.ToString());
+    listaView.SubItems.Add(this.txtApellidos.Text);
+    listaView.SubItems.Add(this.txtNombres.Text);
+    listaView.SubItems.Add(this.txtEdad.Text);
+    listaView.SubItems.Add(this.cmbSexo.Text);
+    lstEstudiante.Items.Add(listaView);
+
+                this.gridEstudiante.DataSource = null;
+                this.gridEstudiante.DataSource = lista;
+                this.gridEstudiante.DataSource = listaGrid;
+
+                this.txtMatricula.Text = "";
+                this.txtApellidos.Text = "";
+@@ -74,10 +80,10 @@ private void btnAgregar_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+    {
+        this.gridEstudiante.DataSource = null;
+        this.gridEstudiante.DataSource = lista.Where(data => data.matricula.ToString() == this.txtFiltrar.Text).ToList();
+        this.gridEstudiante.DataSource = listaGrid.Where(data => data.matricula.ToString() == this.txtFiltrar.Text).ToList();
+
+        this.txtMayor.Text = lista.Max(data => data.edad).ToString();
+        this.txtMenor.Text = lista.Min(data => data.edad).ToString();
+        this.txtMayor.Text = listaGrid.Max(data => data.edad).ToString();
+        this.txtMenor.Text = listaGrid.Min(data => data.edad).ToString();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+@@ -86,5 +92,8 @@ private void Form1_Load(object sender, EventArgs e)
+        private void label6_Click(object sender, EventArgs e)
+    {
+    }
+    private void lstEstudiante_SelectedIndexChanged(object sender, EventArgs e)
+    {
+    }
+}
+}
